2629fd989d1697b9daf85cfbb6197c6f8a01f2ef
njmyers <17010801+njmyers@users.noreply.github.com>
2018-12-20 11:20:08 -0500

    fixed spelling error

--- src/commands/named-function.js
+++ src/commands/named-function.js
@@ -1,5 +1,5 @@
 /**
- * Name an annonymous function
+ * Name an anonymous function
  */
 const namedFunction = (name, fn) => {
   Object.defineProperty(fn, 'name', { value: name, writable: false });
