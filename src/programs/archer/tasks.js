4dab43be1496ae18ed74333792ef8fddf0f4d6a0
njmyers <17010801+njmyers@users.noreply.github.com>
2020-02-20 22:03:32 -0500

    Add constant for file task

--- src/programs/archer/tasks.js
+++ src/programs/archer/tasks.js
@@ -2,6 +2,7 @@ export const PACMAN = 'pacman';
 export const AUR = 'aur';
 export const NPM = 'npm';
 export const SYSTEMCTL = 'systemctl';
+export const FILE = 'file';
 export const LN = 'ln';
 export const UNLINK = 'unlink';
 export const EXEC = 'exec';
